efddcce964172186620962768eb675cca533fb4e
Negishi Kazuki <h2dhe2yc@icloud.com>
2018-12-11 19:29:04 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 devise_for :users
 root 'mains#index'
 
- get 'mains' => 'mains#item_sell'
+get 'mains' => 'mains#item_sell'
+get 'users/create' => 'users#create'
 
 end
