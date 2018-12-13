1cad7f38321464673701f005cd4352488d6f406e
Yukie-Sako <vetkas4astia@gmail.com>
2018-12-13 18:21:28 +0900

    add: association_to_items_model
    *itemsモデルにアソシエーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,8 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_many :images, dependent: :destroy
+  has_one :lcategory, dependent: :destroy
+  has_one :mcategory, dependent: :destroy
+  has_one :scategory, dependent: :destroy
+  has_one :brand, dependent: :destroy
 end
