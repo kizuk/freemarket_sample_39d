132bdb0bddf3f9052902cce02859eda25acd7b86
Yukie-Sako <vetkas4astia@gmail.com>
2018-12-14 12:18:19 +0900

    edit: items.controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,28 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def index
+  end
+
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new, flash[:alert]="必要事項を記入してください"
+    end
   end
 
   def show
   end
+
+  private
+  def item_params
+    params.require(:item).permit(:name, :explain, :lcategories_id, :mcategories_id, :scategories_id, :brands_id, :size, :condition, :shipping_cost, :area, :shipping_date,:price, images_attributes: [:image_url]).merge(user_id: current_user.id)
+  end
 end
+
