6602887810aba3ea7fc76286a2ff5a2a70da68d9
Shigeki Nomachi <shigekinomachi@gmail.com>
2018-12-17 16:42:52 +0900

    Added-users-showpage-tab-change-functione
    User show pageにタブ切り替え機能追加

---
+++ app/assets/javascripts/user.js
@@ -0,0 +1,40 @@
+$(function() {
+
+  // ①タブをクリックしたら発動
+  $('.tab li').click(function() {
+
+    // ②クリックされたタブの順番を変数に格納
+    var index = $('.tab li').index(this);
+
+    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
+    $('.tab li').removeClass('active');
+
+    // ④クリックされたタブにクリック済みデザインを適用する
+    $(this).addClass('active');
+
+    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
+    $('.area ul').removeClass('show').eq(index).addClass('show');
+
+  });
+});
+
+
+$(function() {
+
+  // ①タブをクリックしたら発動
+  $('.tab-2 li').click(function() {
+
+    // ②クリックされたタブの順番を変数に格納
+    var index = $('.tab-2 li').index(this);
+
+    // ③クリック済みタブのデザインを設定したcssのクラスを一旦削除
+    $('.tab-2 li').removeClass('active');
+
+    // ④クリックされたタブにクリック済みデザインを適用する
+    $(this).addClass('active');
+
+    // ⑤コンテンツを一旦非表示にし、クリックされた順番のコンテンツのみを表示
+    $('.area-2 ul').removeClass('show').eq(index).addClass('show');
+
+  });
+});
